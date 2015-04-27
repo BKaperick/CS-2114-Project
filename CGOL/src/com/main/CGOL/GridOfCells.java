b26570f10f5b04d7dba639b64dc2851cbf7bae58
Bryan Kaperick <bryankaperick@gmail.com>
2015-04-27 17:23:30 -0400

    partially implemented save and load features

--- CGOL/src/com/main/CGOL/GridOfCells.java
+++ CGOL/src/com/main/CGOL/GridOfCells.java
@@ -1,11 +1,10 @@
 package com.main.CGOL;
 
-
 /**
  * // -------------------------------------------------------------------------
- * /** Our class for the main game screen.
- * We still intend to implement multiple activity functionality to support
- * a title screen, settings screen and gameplay screen.
+ * /** Our class for the main game screen. We still intend to implement multiple
+ * activity functionality to support a title screen, settings screen and
+ * gameplay screen.
  *
  * @author Parisa Samareh
  * @author Bryan Kaperick
@@ -16,17 +15,21 @@ public class GridOfCells
 
 {
     private CellLocation[][] grid;
-    private int gridWidth;
-    private int gridHeight;
+    private int              gridWidth;
+    private int              gridHeight;
     /**
      * The grid that holds the information for the next generation
      */
-    GridOfCells nextGen;
+    GridOfCells              nextGen;
+
 
     /**
      * The initialize method sets up the grid.
-     * @param x     horizontal dimension of play area
-     * @param y     vertical dimension of play area
+     *
+     * @param x
+     *            horizontal dimension of play area
+     * @param y
+     *            vertical dimension of play area
      */
     public GridOfCells(int x, int y)
     {
@@ -43,6 +46,7 @@ public class GridOfCells
         }
     }
 
+
     /**
      * Returns this grid
      *
@@ -53,18 +57,22 @@ public class GridOfCells
         return grid;
     }
 
+
     /**
      * Defines this grid
      *
-     * @param newGrid the grid we are equating it to
+     * @param newGrid
+     *            the grid we are equating it to
      */
     public void setGrid(CellLocation[][] newGrid)
     {
         grid = newGrid;
     }
 
+
     /**
      * Returns the height of the total grid.
+     *
      * @return height field value.
      */
     public int height()
@@ -72,8 +80,10 @@ public class GridOfCells
         return gridHeight;
     }
 
+
     /**
      * Returns the width of the total grid.
+     *
      * @return width field value.
      */
     public int width()
@@ -85,8 +95,10 @@ public class GridOfCells
     /**
      * Gets the status of the cell's neighbors
      *
-     * @param x coordinate of the cell
-     * @param y coordinate of the cell
+     * @param x
+     *            coordinate of the cell
+     * @param y
+     *            coordinate of the cell
      * @return the number of neighbors
      */
     public int getNeighbors(int x, int y)
@@ -96,8 +108,8 @@ public class GridOfCells
         {
             for (int j = y - 1; j < y + 2; j++)
             {
-                int p = i-x;
-                int q = j-y;
+                int p = i - x;
+                int q = j - y;
                 System.out.println(p + " " + " " + q);
                 if (!(x == i && y == j))
                 {
@@ -158,41 +170,87 @@ public class GridOfCells
         }
     }
 
+
     /**
      * Updates the grid cells depending on their state of living
-     * @param i the x coord
-     * @param j the y coord
+     *
+     * @param i
+     *            the x coord
+     * @param j
+     *            the y coord
      * @return the boolean of whether or not the cell is alive
      */
     public boolean update(int i, int j)
     {
         if (this.getCell(i, j).getAlive())
         {
-            if(this.getNeighbors(i, j) < 2 || this.getNeighbors(i, j) > 3)
+            if (this.getNeighbors(i, j) < 2 || this.getNeighbors(i, j) > 3)
             {
-                System.out.println("coords: "+i+" "+j+" alive 2-3 neighbors "+getNeighbors(i, j));
+                System.out.println("coords: " + i + " " + j
+                    + " alive 2-3 neighbors " + getNeighbors(i, j));
                 return false;
             }
             else
             {
-                System.out.println("coords: "+i+" "+j+" alive 0-1or4- neighbors "+getNeighbors(i, j));
+                System.out.println("coords: " + i + " " + j
+                    + " alive 0-1or4- neighbors " + getNeighbors(i, j));
                 return true;
             }
         }
         else
         {
-            if(this.getNeighbors(i, j) == 3)
+            if (this.getNeighbors(i, j) == 3)
             {
-                System.out.println("coords: "+i+" "+j+" dead 3 neighbors "+getNeighbors(i, j));
+                System.out.println("coords: " + i + " " + j
+                    + " dead 3 neighbors " + getNeighbors(i, j));
                 return true;
             }
             else
             {
-                System.out.println("coords: "+i+" "+j+" dead not 3 neighbors "+getNeighbors(i, j));
+                System.out.println("coords: " + i + " " + j
+                    + " dead not 3 neighbors " + getNeighbors(i, j));
                 return false;
             }
         }
 
+    }
+
+
+    public String gridToSaveFormat()
+    {
+        String output = "";
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                if (isAlive(i, j))
+                {
+                    output += "1";
+                }
+                else
+                {
+                    output += "0";
+                }
+            }
+            output += "n";
+        }
+        return output;
+    }
+
+    /**
+     * Saving and loading features are not finished.  IE, this method is not finished.
+     */
+    public void setGridFromSaveFormat(String save)
+    {
+        int newWidth = save.substring(0, save.indexOf('n')).length();
+        int newHeight = save.length() - save.replace("n", "").length();
+
+        CellLocation[][] newGrid = new CellLocation[newWidth][newHeight];
+        for (int i = 0; i < save.length(); i++)
+        {
+            newGrid[i][j] =
+
+        }
     }
 
 }
